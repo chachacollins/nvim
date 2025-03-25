db384b8ba3d110507de24409fe35c78766bc6127
chachacollins <collinschacha@hotmail.com>
2025-03-25 11:23:27 +0300

    something

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,22 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release
+  lazy = true,
+  ft = 'markdown',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('obsidian').setup {
+      workspaces = {
+        {
+          name = 'personal',
+          path = vim.fn.expand '~/Documents/chacha/',
+        },
+      },
+
+      -- Additional configuration to ensure loading
+      ui = { enable = false },
+    }
+  end,
+}
